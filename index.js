42d8002459c5e75aca2d0e1b418ceeae87b2ceb6
John Otander <johnotander@gmail.com>
2017-12-23 15:59:09 -0800

    Reorganize module

--- index.js
+++ index.js
@@ -1,46 +1,13 @@
-import unified from 'unified'
-import parse from 'remark-parse'
+import remark from 'remark'
 import toHast from 'mdast-util-to-hast'
 import toHyper from 'hast-to-hyperscript'
 
 import { createElement } from 'react'
 
-import isLiveEditor from './lib/is-live-editor'
-import LiveEditor from './lib/LiveEditor'
+import transformer from './lib/transformer'
 
-const markdown = (md, options = {}) => {
-  const mdComponents = options.mdComponents || {}
-  const components = options.components || {}
-  const theme = options.theme || {}
-
-  const transformLiveEditor = (node, components) => {
-    const children = node.children || []
-    const code = (children[0] || {}).value || ''
-
-    const props = Object.assign({}, node.properties, {
-      components,
-      theme,
-      code
-    })
-
-    return createElement(LiveEditor, props, code)
-  }
-
-  const transform = node => {
-    const component = mdComponents[node.tagName] || node.tagName
-
-    return createElement(component, node.properties, node.children)
-  }
-
-  const compile = node => isLiveEditor(node)
-    ? transformLiveEditor(node, components)
-    : transform(node)
-
-  const tree = unified()
-    .use(parse)
-    .parse(md)
-
-  return toHast(tree).children.map(compile)
-}
-
-module.exports = markdown
+module.exports = (md, options = {}) =>
+  remark()
+    .use(transformer, options)
+    .processSync(md)
+    .contents
--- test/fixtures/basic.md
+++ test/fixtures/basic.md
@@ -1,3 +1,23 @@
+# Hello, world
+
+This is an example markdown doc.
+
+## Installation
+
+```sh
+npm i -g foo
+```
+
+## Usage
+
+```
+import Foo from 'bar'
+
+export default props => <Foo {...props} />
+```
+
+The following is a live editor code block:
+
 ```.jsx
 <h1>Hello, world!</h1>
 ```
