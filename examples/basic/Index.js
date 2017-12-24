dcc5f2271682e3910d1c05bec014b877c011868f
John Otander <johnotander@gmail.com>
2017-12-24 11:21:47 -0800

    Implement live editor portion

---
+++ examples/basic/Index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import content from './markdown'
+import markdown from '../../'
+
+
+export default () => <div>{markdown(content)}</div>
---
+++ examples/basic/markdown.js
@@ -0,0 +1,25 @@
+export default `
+# Hello, world
+
+This is an example markdown doc.
+
+## Installation
+
+\`\`\`sh
+npm i -g foo
+\`\`\`
+
+## Usage
+
+\`\`\`
+import Foo from 'bar'
+
+export default props => <Foo {...props} />
+\`\`\`
+
+The following is a live editor code block:
+
+\`\`\`.jsx
+<h1>Hello, world!</h1>
+\`\`\`
+`
