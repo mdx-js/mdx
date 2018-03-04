112386ffc7713ae38f1a62af3b5cb40ddb8b7685
John Otander <johnotander@gmail.com>
2018-03-04 10:01:18 -0700

    Ensure only image url

--- remark-images/index.js
+++ remark-images/index.js
@@ -1,11 +1,13 @@
 const isUrl = require('is-url')
 const visit = require('unist-util-visit')
 
+const isImgUrl = str => /\.(svg|png|jpg|jpeg)/.test(str)
+
 module.exports = () => (tree, file) =>
   visit(tree, 'text', node => {
     const text = node.value ? node.value.trim() : ''
 
-    if (!isUrl(text)) {
+    if (!isUrl(text) || !isImgUrl(text)) {
       return
     }
 
