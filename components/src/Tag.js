747a39f2b707da81ec894f54acdf8161e8146738
Tim Neutkens <timneutkens@me.com>
2018-03-08 23:39:30 +0100

    Provide default if no components, wrapper needs to become div if not provided

--- components/src/Tag.js
+++ components/src/Tag.js
@@ -6,9 +6,9 @@ class Tag extends React.Component {
     components: PropTypes.Object
   }
   render() {
-    const {components} = this.context
+    const {components = {}} = this.context
     const {name, props, children} = this.props
-    const Component = components[name] || name
+    const Component = components[name] || name === 'wrapper' ? 'div' : name
     return <Component {...props}>{children}</Component>
   }
 }
