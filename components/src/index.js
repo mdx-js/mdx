3537ac6f22f6c7836b3a1909f63709932e702f44
Tim Neutkens <timneutkens@me.com>
2018-03-12 12:55:32 +0100

    Remove markdown and provider

--- components/src/index.js
+++ components/src/index.js
@@ -1,11 +1,7 @@
-import Markdown from './Markdown'
-import ComponentsProvider from './Provider'
 import withIdLink from './withIdLink'
 import Tag from './Tag'
 
 export {
   Tag,
-  Markdown,
-  ComponentsProvider,
   withIdLink
 }
