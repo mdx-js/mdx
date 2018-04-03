9de4482ac619742a8315e90eb4783e9e761c1619
John Otander <johnotander@gmail.com>
2018-04-03 09:39:43 -0600

    Update to latest api

--- examples/next/pages/index.js
+++ examples/next/pages/index.js
@@ -2,14 +2,7 @@ import React from 'react'
 import Document from '../md/markdown.md'
 
 const H1 = props => <h1 style={{ color: 'tomato' }} {...props} />
-const Box = props => <div style={{ border: 'thin solid purple' }} {...props} />
 
 export default () => (
-  <Document
-    components={{
-      h1: H1,
-      Foo: H1,
-      Box
-    }}
-  />
+  <Document components={{ h1: H1 }} />
 )
