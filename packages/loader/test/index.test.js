db3769d376e1e17ca97554f83830cbeaa8d2f353
John Otander <johnotander@gmail.com>
2018-04-15 10:01:30 -0600

    Implement basic loader test

---
+++ packages/loader/test/index.test.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const webpack = require('webpack')
+const memoryFs = require('memory-fs')
+
+const testFixture = (fixture, options = {}) => {
+  const fileName = `./${fixture}`
+
+  const compiler = webpack({
+    context: __dirname,
+    entry: `./${fixture}`,
+    output: {
+      path: path.resolve(__dirname),
+      filename: 'bundle.js'
+    },
+    node: {
+      fs: 'empty'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.md?$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['env', 'stage-0', 'react']
+              }
+            },
+            {
+              loader: path.resolve(__dirname, '..')
+            }
+          ]
+        }
+      ]
+    }
+  })
+
+  compiler.outputFileSystem = new memoryFs()
+
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      if (err) reject(err)
+
+      const module = stats.toJson().modules.find(m => m.name === fileName)
+        .source
+
+      resolve(module)
+    })
+  })
+}
+
+test('it loads markdown and returns a component', async () => {
+  const result = await testFixture('fixture.md')
+
+  expect(result).toMatchSnapshot()
+})
