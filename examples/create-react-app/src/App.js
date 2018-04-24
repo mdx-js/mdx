18e21b51bb1ba3b62203b5a989ff7711b09416a8
Tim Neutkens <timneutkens@me.com>
2018-04-23 19:30:14 -0700

    Add create-react-app example

---
+++ examples/create-react-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Test from './test.mdx'
+
+console.log(Test)
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Test />
+      </div>
+    );
+  }
+}
+
+export default App;
