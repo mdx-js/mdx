3ca4c82dce49db2f781a605dfd8f123cf32e5a0c
Michael Novotny <manovotny@gmail.com>
2018-04-26 19:44:48 -0500

    Adds ability to render absolute or relative image paths to remark-images.

--- packages/remark-images/index.js
+++ packages/remark-images/index.js
@@ -1,18 +1,19 @@
 const isUrl = require('is-url')
 const visit = require('unist-util-visit')
 
-const isImgUrl = str => /\.(svg|png|jpg|jpeg)$/.test(str)
+const isImgExt = str => /\.(svg|png|jpg|jpeg)$/.test(str)
+const isAbsolutePath = str => str.startsWith('/')
+const isRelativePath = str => str.startsWith('./') || str.startsWith('../')
+const isImgPath = str => isAbsolutePath(str) || isRelativePath(str)
 
-module.exports = () => (tree, file) =>
+module.exports = () => (tree) =>
   visit(tree, 'text', node => {
     const text = node.value ? node.value.trim() : ''
 
-    if (!isUrl(text) || !isImgUrl(text)) {
-      return
+    if ((isUrl(text) || isImgPath(text)) && isImgExt(text)) {
+      node.type = 'image'
+      node.url = text
+
+      delete node.value
     }
-
-    node.type = 'image'
-    node.url = text
-
-    delete node.value
   })
