0bab218b99044595f97023acac2f20662adb6e54
Adam Putinski <adam.putinski@gmail.com>
2018-06-05 10:47:37 -0400

    fix(mdx): add "components" prop to wrapper

--- packages/tag/test/test.js
+++ packages/tag/test/test.js
@@ -1,8 +1,11 @@
 import fs from 'fs'
 import test from 'ava'
 import React from 'react'
-import { renderToString } from 'react-dom/server'
+import { renderToString, renderToStaticMarkup } from 'react-dom/server'
+import vm from 'vm'
+import * as babel from 'babel-core'
 
+import mdx from '../../mdx'
 import { MDXTag } from '../src'
 
 const H1 = props => <h1 style={{ color: 'tomato' }} {...props} />
@@ -13,4 +16,19 @@ test('renders the desired component', t => {
   )
 
   t.snapshot(result)
+})
+
+test('renders the desired component (wrapper)', t => {
+  const jsx = mdx.sync('# Hello')
+  const { code } = babel.transform(jsx, {
+    presets: ['react']
+  })
+  const Markdown = vm.runInContext(
+    code.replace(/export default/, ''),
+    vm.createContext({ React, MDXTag })
+  )
+  const html = renderToStaticMarkup(
+    <Markdown components={{ wrapper: React.Fragment }} />
+  )
+  t.is(html, '<h1>Hello</h1>')
 })
