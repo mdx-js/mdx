0f01ff13262f23bd0837ea4f1fbe56fc39a2c53f
Frank Lyder Bredland <fl@nyggen.com>
2018-06-23 15:54:04 +0200

    Work towards issue #166
    This allows you to change out imports for the mdx-loader.

--- packages/loader/index.js
+++ packages/loader/index.js
@@ -7,6 +7,9 @@ module.exports = async function(content) {
 
   const result = await mdx(content, options || {})
 
+  if(typeof options !== "undefined" && typeof options.process == "function")
+    return callback(null, options.process(result))
+
   const code = `
   import React from 'react'
   import { MDXTag } from '@mdx-js/tag'
