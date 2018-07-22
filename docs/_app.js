c5b8e9d824acebfbc279957a7c68985105f15122
John Otander <johnotander@gmail.com>
2018-07-22 11:30:23 -0600

    Add live editor for MDX in docs

--- docs/_app.js
+++ docs/_app.js
@@ -1,10 +1,46 @@
 import React from 'react'
 import RebassMDX from '@rebass/mdx'
+import createScope from '@rebass/markdown'
 import * as Rebass from 'rebass'
 import sortBy from 'lodash.sortby'
 import { Link } from 'react-router-dom'
-import { SidebarLayout as Layout } from '@compositor/x0/components'
-import { Flex, Box, Container } from 'rebass'
+import { Flex, Box, Container, Pre } from 'rebass'
+import { SidebarLayout as Layout, ScopeProvider, LiveEditor, LivePreview } from '@compositor/x0/components'
+
+const Editor = props => {
+  const lang = props.className.replace(/^language\-/, '')
+  const type = lang.charAt(0)
+  const code = React.Children.toArray(props.children).join('\n')
+
+  switch (type) {
+    case '.':
+      return (
+        <LiveEditor
+          mdx={lang === '.mdx'}
+          code={code}
+        />
+      )
+    case '!':
+      return (
+        <LivePreview
+          mdx={lang === '!mdx'}
+          code={code}
+        />
+      )
+    default:
+      return (
+        <Pre
+          p={3}
+          mt={4}
+          mb={4}
+          bg='gray'
+          children={props.children}
+        />
+      )
+  }
+}
+
+const scope = { ...createScope(), ...Rebass, code: Editor, pre: ({ children }) => children }
 
 const navOrder = [
   'index',
@@ -55,11 +91,13 @@ export default class App extends React.Component {
     const nav = sortRoutes(routes)
 
     return (
-      <RebassMDX components={Rebass}>
-        <Layout
-          {...this.props}
-          routes={nav}
-        />
+      <RebassMDX>
+        <ScopeProvider scope={scope}>
+          <Layout
+            {...this.props}
+            routes={nav}
+          />
+        </ScopeProvider>
       </RebassMDX>
     )
   }
