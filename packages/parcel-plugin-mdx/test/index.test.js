25a9aa89ba28063427c3b18a84de03a2b902891c
Trey Shugart <treshugart@gmail.com>
2018-08-08 13:51:02 +1000

    Fix importing .md files.

--- packages/parcel-plugin-mdx/test/index.test.js
+++ packages/parcel-plugin-mdx/test/index.test.js
@@ -1,7 +1,24 @@
 const path = require('path')
+const index = require('../src');
 const MDXAsset = require('../src/MDXAsset')
 
 let value
+
+describe('index', () => {
+  it('should add asset types', () => {
+    const bundler = { addAssetType: jest.fn() }
+    const { calls } = bundler.addAssetType.mock
+    const mdxAssetPath = require.resolve('../src/MDXAsset')
+
+    index(bundler)
+
+    expect(calls.length).toEqual(2)
+    expect(calls[0][0]).toBe('md')
+    expect(calls[0][1]).toBe(mdxAssetPath)
+    expect(calls[1][0]).toEqual('mdx')
+    expect(calls[1][1]).toEqual(mdxAssetPath)
+  })
+})
 
 describe('MDXAsset', async () => {
   beforeEach(async () => {
