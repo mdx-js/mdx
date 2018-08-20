9b0473aeb04d0dbea5acb6845b02161b58ae2bcc
John Otander <johnotander@gmail.com>
2018-08-19 18:06:58 -0600

    Runtime skip export (#229)
    * Use skipExports option
    
    * Remove unneeded quote syntax

--- packages/runtime/src/index.js
+++ packages/runtime/src/index.js
@@ -11,11 +11,7 @@ export default ({ scope = {}, components = {}, children, ...props }) => {
     ...scope
   }
 
-  // We should add this as an output option to mdx core so we don't
-  // need to do hacky regexing.
-  const jsx = mdx
-    .sync(children)
-    .replace(/^(\s)*export default \({components, ...props}\) =>/, '')
+  const jsx = mdx.sync(children, { skipExport: true }).trim()
 
   const { code } = transform(jsx)
 
