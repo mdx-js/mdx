2442104b9315af934357800e0a7e6a8c68355bab
ChristopherBiscardi <chris@christopherbiscardi.com>
2018-09-12 11:48:19 -0700

    enable infostring props

--- packages/mdx/mdx-ast-to-mdx-hast.js
+++ packages/mdx/mdx-ast-to-mdx-hast.js
@@ -1,4 +1,6 @@
 const toHAST = require('mdast-util-to-hast')
+const detab = require('detab')
+const u = require('unist-builder')
 
 module.exports = function mdxAstToMdxHast() {
   return (tree, file) => {
@@ -17,6 +19,38 @@ module.exports = function mdxAstToMdxHast() {
             }
           ]
         })
+      },
+      code(h, node) {
+        var value = node.value ? detab(node.value + '\n') : ''
+        var lang = node.lang && node.lang.match(/^[^ \t]+(?=[ \t]|$)/)
+        var props = {}
+
+        if (lang) {
+          props.className = ['language-' + lang]
+        }
+
+        props.metaString =
+          node.lang && node.lang.replace(/^[^ \t]+(?=[ \t]|$)/, '').trim()
+
+        const meta =
+          props.metaString &&
+          props.metaString.split(' ').reduce((acc, cur) => {
+            if (cur.split('=').length > 1) {
+              const t = cur.split('=')
+              acc[t[0]] = t[1]
+              return acc
+            } else {
+              acc[cur] = true
+              return acc
+            }
+          }, {})
+
+        meta &&
+          Object.entries(meta).forEach(([key, value]) => (props[key] = value))
+
+        return h(node.position, 'pre', [
+          h(node, 'code', props, [u('text', value)])
+        ])
       },
       import(h, node) {
         return Object.assign({}, node, {
