ba74efd7640913b8520743f7ce0802e37ff212b7
Hugo Manrique <hugmanrique@users.noreply.github.com>
2018-11-03 21:55:06 +0100

    Fix metastring prop test

--- packages/mdx/test/index.test.js
+++ packages/mdx/test/index.test.js
@@ -82,7 +82,7 @@ COPY start.sh /home/start.sh
   )
 
   expect(result).toContain(
-    `props={{"className":"language-dockerfile","exec":true,"registry":"something.com"}}`
+    `props={{"className":"language-dockerfile","metastring":"exec registry=something.com","exec":true,"registry":"something.com"}}`
   )
 })
 
