78d65374e34aa7755805d06dd6a9bb6186cb99a5
Matija Marohnić <matija.marohnic@gmail.com>
2018-11-06 17:37:18 +0100

    Fix ESLint errors

--- packages/mdx/mdx-ast-to-mdx-hast.js
+++ packages/mdx/mdx-ast-to-mdx-hast.js
@@ -29,7 +29,7 @@ module.exports = function mdxAstToMdxHast() {
         if (lang) {
           props.className = ['language-' + lang]
         }
-        
+
         props.metastring = node.lang && node.lang.replace(langRegex, '').trim()
 
         const meta =
