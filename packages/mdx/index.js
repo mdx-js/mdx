57ba6947c301765d49ccf612639a36c6d552a012
Titus Wormer <tituswormer@gmail.com>
2018-11-22 22:24:02 +0100

    Remove `default` field from `import` nodes
    Closes GH-313.

--- packages/mdx/index.js
+++ packages/mdx/index.js
@@ -25,12 +25,16 @@ const tokenizeEsSyntax = (eat, value) => {
   const index = value.indexOf(EMPTY_NEWLINE)
   const subvalue = index !== -1 ? value.slice(0, index) : value
 
-  if (isExport(subvalue) || isImport(subvalue)) {
+  if (isExport(subvalue)) {
     return eat(subvalue)({
-      type: isExport(subvalue) ? 'export' : 'import',
+      type: 'export',
       default: isExportDefault(subvalue),
       value: subvalue
     })
+  }
+
+  if (isImport(subvalue)) {
+    return eat(subvalue)({type: 'import', value: subvalue})
   }
 }
 
