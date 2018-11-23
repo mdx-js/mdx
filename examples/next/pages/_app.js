599bf1c46c5b5c98e2d5e0f83cacfc6850d61faa
John Otander <johnotander@gmail.com>
2018-11-23 11:43:47 -0800

    Use style tag in layout for base styles

--- examples/next/pages/_app.js
+++ examples/next/pages/_app.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import {MDXProvider} from '@mdx-js/tag'
-import {createGlobalStyle} from 'styled-components'
-import {Provider, baseStyles, mdComponents} from 'unified-ui'
-
-// eslint-disable-next-line no-unused-expressions
-createGlobalStyle`
-  ${baseStyles}
-`
+import {Provider, mdComponents} from 'unified-ui'
 
 export default ({Component, pageProps}) => (
   <MDXProvider components={mdComponents}>
