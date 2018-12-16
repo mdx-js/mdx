78b50ce5762f988e08b6ade2f084e6a6b6507e69
John Otander <johnotander@gmail.com>
2018-12-16 14:13:52 -0700

    Add basic inline parsing test suite

---
+++ packages/remark-mdx/test/inline-parsing.test.js
@@ -0,0 +1,30 @@
+const unified = require('unified')
+const remarkParse = require('remark-parse')
+const remarkStringify = require('remark-stringify')
+const remarkMDX = require('..')
+
+const fixtures = require('./fixtures')
+
+function jsxCompiler() {
+  this.Compiler.prototype.visitors.jsx = node => node.value
+}
+
+const parse = mdx => {
+  const result = unified()
+    .use(remarkParse)
+    .use(remarkMDX)
+    .use(remarkStringify)
+    .use(jsxCompiler)
+    .processSync(mdx)
+
+  return result.contents
+}
+
+describe('inline JSX parsing', () => {
+  fixtures.forEach(fixture => {
+    it(fixture.description, () => {
+      const result = parse(fixture.mdx)
+      expect(result.trim()).toEqual(fixture.mdx)
+    })
+  })
+})
