8091d58c7132102083e0d8552e16e1e3933af496
John Otander <johnotander@gmail.com>
2018-12-17 06:38:10 -0700

    Begin attempting to handle object props

--- packages/remark-mdx/tag.js
+++ packages/remark-mdx/tag.js
@@ -1,12 +1,23 @@
-// Source copied from https://github.com/remarkjs/remark/blob/master/packages/remark-parse/lib/util/html.js
+// Source copied and then modified from
+// https://github.com/remarkjs/remark/blob/master/packages/remark-parse/lib/util/html.js
+//
 // MIT License https://github.com/remarkjs/remark/blob/master/license
 
 const attributeName = '[a-zA-Z_:][a-zA-Z0-9:._-]*'
 const unquoted = '[^"\'=<>`\\u0000-\\u0020]+'
 const singleQuoted = "'[^']*'"
 const doubleQuoted = '"[^"]*"'
+const objectProps = '{{[*]}}'
 const attributeValue =
-  '(?:' + unquoted + '|' + singleQuoted + '|' + doubleQuoted + ')'
+  '(?:' +
+  unquoted +
+  '|' +
+  singleQuoted +
+  '|' +
+  doubleQuoted +
+  '|' +
+  objectProps +
+  ')'
 const attribute =
   '(?:\\s+' + attributeName + '(?:\\s*=\\s*' + attributeValue + ')?)'
 const openTag = '<[A-Za-z]*[A-Za-z0-9\\-]*' + attribute + '*\\s*\\/?>'
