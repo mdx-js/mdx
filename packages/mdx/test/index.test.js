15d196c48fa816aa2933180b251e5a4937109b24
John Otander <johnotander@gmail.com>
2019-02-14 22:21:07 +0100

    Change isMDXComponent type to bool
    Co-Authored-By: hugmanrique <hugmanrique@users.noreply.github.com>

--- packages/mdx/test/index.test.js
+++ packages/mdx/test/index.test.js
@@ -292,7 +292,7 @@ it('Should recognize components as properties', async () => {
 
 it('Should contain static isMDXComponent() function', async () => {
   const result = await mdx('# Hello World')
-  expect(result).toContain('MDXContent.isMDXComponent = () => true')
+  expect(result).toContain('MDXContent.isMDXComponent = true')
 })
 
 it('Should render elements without wrapping blank new lines', async () => {
