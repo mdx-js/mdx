1742171b9fa8ecb2d6678a10ac554dea935f9d67
Christopher Biscardi <biscarch@sketcht.com>
2019-02-26 02:45:58 -0800

    [remove this] more my-site examples

--- my-site-2/gatsby-browser.js
+++ my-site-2/gatsby-browser.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import {MDXProvider} from '@mdx-js/tag'
 
-const components = {h1: props => <h5 {...props} />}
+const components = {
+  h1: props => <h5 {...props} />,
+  aside: props => <pre {...props} />
+}
 
 export const wrapRootElement = ({element}) => {
   return <MDXProvider components={components}>{element}</MDXProvider>
