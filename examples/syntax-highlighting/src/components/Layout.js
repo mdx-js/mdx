b6ebc95a54c33d334b01175b4168a2860e541939
John Otander <johnotander@gmail.com>
2019-03-02 15:20:21 -0700

    Add example for syntax highlighting and live editor
    Fixes #253

--- examples/syntax-highlighting/src/components/Layout.js
+++ examples/syntax-highlighting/src/components/Layout.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import {MDXProvider} from '@mdx-js/tag'
 import {Container, baseStyles} from 'unified-ui'
+
+import CodeBlock from './CodeBlock'
 
 const Style = ({children}) => (
   <style
@@ -9,9 +12,17 @@ const Style = ({children}) => (
   />
 )
 
+const components = {
+  h1: props => <h1 style={{color: 'tomato'}} {...props} />,
+  pre: props => <div {...props} />,
+  code: CodeBlock
+}
+
 export default props => (
-  <>
-    <Style>{baseStyles}</Style>
-    <Container {...props} />
-  </>
+  <MDXProvider components={components}>
+    <>
+      <Style>{baseStyles}</Style>
+      <Container {...props} />
+    </>
+  </MDXProvider>
 )
