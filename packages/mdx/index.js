c241279f53fdd3dc644bdca3bac473cf5f871b36
John Otander <johnotander@gmail.com>
2019-03-05 21:04:02 -0700

    Use remark-mdx in core as a syntactic extension to remark (#439)
    * Implement support for Some.Component style JSX blocks
    
    * Add failing test for string interpolation
    
    * Add failing test for js functions as props
    
    * Add failing test for js functions as props with returns
    
    * Make value parsing for JSX props more flexible
    
    * Don't lint fixtures file
    
    * Add nested object props fixture
    
    * Add another fixture for randomly placed brackets
    
    * Add link shortcut to fixture
    
    * Remove object props since it is a subset of js props
    
    * Begin implementing core remark-mdx usage
    
    * Replace MDXTag with custom pragma (#401)
    
    * it works
    
    * commit to be removed: my-site-2
    
    * remove mdxtag for layout
    
    * [remove this] more my-site examples
    
    * remove
    
    * merge-conflict
    
    * snapshots
    
    * Remove my-site-2
    
    * Clean up create element code
    
    * Fix tests
    
    * Fix props handling, remove mdx pragma from JSX output
    
    * Update pragma implementation in loader and parcel plugin
    
    * Update runtime to properly provide components for mdx pragma
    
    * Allow for merging of components passed to context with a function (#411)
    
    If a function is passed to the theme provider it is invoked
    with the out context's components. This allows users to opt
    in to merging those components.
    
    Related #410
    
    * feat: Convert withMDXComponents to use hooks (#417)
    
    * Continue working on integrating remark-mdx
    
    * Ensure proper remark-mdx is used for workspaces

--- packages/mdx/index.js
+++ packages/mdx/index.js
@@ -1,5 +1,6 @@
 const unified = require('unified')
 const toMDAST = require('remark-parse')
+const remarkMdx = require('remark-mdx')
 const squeeze = require('remark-squeeze-paragraphs')
 const visit = require('unist-util-visit')
 const raw = require('hast-util-raw')
@@ -7,51 +8,11 @@ const toMDXAST = require('./md-ast-to-mdx-ast')
 const mdxAstToMdxHast = require('./mdx-ast-to-mdx-hast')
 const mdxHastToJsx = require('./mdx-hast-to-jsx')
 
-const {
-  isImport,
-  isExport,
-  isExportDefault,
-  BLOCKS_REGEX,
-  EMPTY_NEWLINE
-} = require('./util')
-
 const DEFAULT_OPTIONS = {
   footnotes: true,
   mdPlugins: [],
   hastPlugins: [],
-  compilers: [],
-  blocks: [BLOCKS_REGEX]
-}
-
-const tokenizeEsSyntax = (eat, value) => {
-  const index = value.indexOf(EMPTY_NEWLINE)
-  const subvalue = index !== -1 ? value.slice(0, index) : value
-
-  if (isExport(subvalue)) {
-    return eat(subvalue)({
-      type: 'export',
-      default: isExportDefault(subvalue),
-      value: subvalue
-    })
-  }
-
-  if (isImport(subvalue)) {
-    return eat(subvalue)({type: 'import', value: subvalue})
-  }
-}
-
-tokenizeEsSyntax.locator = (value, _fromIndex) => {
-  return isExport(value) || isImport(value) ? -1 : 1
-}
-
-function esSyntax() {
-  const Parser = this.Parser
-  const tokenizers = Parser.prototype.blockTokenizers
-  const methods = Parser.prototype.blockMethods
-
-  tokenizers.esSyntax = tokenizeEsSyntax
-
-  methods.splice(methods.indexOf('paragraph'), 0, 'esSyntax')
+  compilers: []
 }
 
 function createMdxAstCompiler(options) {
@@ -59,7 +20,7 @@ function createMdxAstCompiler(options) {
 
   const fn = unified()
     .use(toMDAST, options)
-    .use(esSyntax)
+    .use(remarkMdx, options)
     .use(squeeze, options)
     .use(toMDXAST, options)
 
