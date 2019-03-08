5d33f7b2aa5729914a9a85ff76f53875cb405953
John Otander <johnotander@gmail.com>
2019-03-07 22:11:12 -0700

    Spike out vue example using vue cli

---
+++ examples/vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+//import Test from './test.mdx'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+//    Test
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
