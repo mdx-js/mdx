996c9ece3e25966c249c75460d62314f42075365
Christopher Biscardi <biscarch@sketcht.com>
2019-03-08 17:44:55 -0800

    vue works

--- packages/vue/src/create-element.js
+++ packages/vue/src/create-element.js
@@ -1,11 +1,3 @@
-import Vue from 'vue'
-
-export default (type, props, children) => {
-  console.log({type, props, children})
-
-  return Vue.component('mdx-component', {
-    render: function(createElement) {
-      return createElement(type, props, children)
-    }
-  })
+export default function(type, props, children) {
+  return this.vueCreateElement(type, props, children)
 }
