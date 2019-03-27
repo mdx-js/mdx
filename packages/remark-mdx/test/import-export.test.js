bc4a339331d0710644cd06a4ef24a778ce9ad926
John Otander <johnotander@gmail.com>
2019-03-27 14:30:50 -0600

    Fix test suite

--- packages/remark-mdx/test/import-export.test.js
+++ packages/remark-mdx/test/import-export.test.js
@@ -4,7 +4,10 @@ const vfile = require('vfile')
 
 fixtures.forEach(fixture => {
   it(fixture.description, () => {
-    const result = extract(fixture.mdx, vfile({ path: '/test', content: 'testing' }))
+    const result = extract(
+      fixture.mdx,
+      vfile({path: '/test', content: 'testing'})
+    )
 
     expect(result).toMatchSnapshot(fixture.result)
   })
