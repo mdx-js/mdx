ffce42f1cd87394b49f8be9a2bf2acc0d65ef42f
John Otander <johnotander@gmail.com>
2019-04-11 16:17:49 -0600

    Update snapshots

--- packages/mdx/test/index.test.js
+++ packages/mdx/test/index.test.js
@@ -114,39 +114,7 @@ it('Should match sample blog post snapshot', async () => {
     
     const layoutProps = {};
     const MDXLayout = \\"wrapper\\";
-    export default function _objectWithoutProperties(source, excluded) {
-      if (source == null) return {};
-      var target = _objectWithoutPropertiesLoose(source, excluded);
-      var key, i;
-      if (Object.getOwnPropertySymbols) {
-        var sourceSymbolKeys = Object.getOwnPropertySymbols(source);
-        for (i = 0; i < sourceSymbolKeys.length; i++) {
-          key = sourceSymbolKeys[i];
-          if (excluded.indexOf(key) >= 0) continue;
-          if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue;
-          target[key] = source[key];
-        }
-      }
-      return target;
-    }
-    
-    function _objectWithoutPropertiesLoose(source, excluded) {
-      if (source == null) return {};
-      var target = {};
-      var sourceKeys = Object.keys(source);
-      var key, i;
-      for (i = 0; i < sourceKeys.length; i++) {
-        key = sourceKeys[i];
-        if (excluded.indexOf(key) >= 0) continue;
-        target[key] = source[key];
-      }
-      return target;
-    }
-    
-    function MDXContent(_ref) {
-      let { components } = _ref,
-        props = _objectWithoutProperties(_ref, [\\"components\\"]);
-    
+    export default function MDXContent({ components, ...props }) {
       return (
         <MDXLayout
           {...layoutProps}
@@ -441,16 +409,10 @@ test('Should handle layout props', () => {
       foo
     };
     const MDXLayout = ({children}) => <div>{children}</div>
-    export default function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-    
-    function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-    
-    function MDXContent(_ref) {
-      let {
-        components
-      } = _ref,
-          props = _objectWithoutProperties(_ref, [\\"components\\"]);
-    
+    export default function MDXContent({
+      components,
+      ...props
+    }) {
       return <MDXLayout {...layoutProps} {...props} components={components} mdxType=\\"MDXLayout\\">
     
     
