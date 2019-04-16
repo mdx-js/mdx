145c8e7dbae6f4464f42381ddd8c8168dfde8462
Jarred Sumner <jarred@jarredsumner.com>
2019-04-15 17:22:13 -0700

    Add tests for faking browser environment, to ensure @babel/standalone works correctly

---
+++ packages/remark-mdx/test/import-export.browser.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const extract = require('../dist/extract-imports-and-exports')
+const fixtures = require('./fixtures/import-export')
+const vfile = require('vfile')
+
+fixtures.forEach(fixture => {
+  it(fixture.description, () => {
+    const result = extract(
+      fixture.mdx,
+      vfile({path: '/test', content: 'testing'})
+    )
+
+    expect(result).toMatchSnapshot(fixture.result)
+  })
+})
