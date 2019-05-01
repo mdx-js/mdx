523833aa0ee4afc67c53ccbcccbfc139cda6f834
John Otander <johnotander@gmail.com>
2019-05-01 15:01:55 -0600

    Add button to initial value

--- packages/gatsby-theme-mdx/src/components/playground.js
+++ packages/gatsby-theme-mdx/src/components/playground.js
@@ -6,7 +6,9 @@ import PlaygroundEditor from '../components/playground-editor'
 export default () => {
   const initialValue =
     typeof window !== 'undefined' && window.localStorage.lastMDXPlayground
-  const [code, setCode] = useState(initialValue || '# Hello, world!')
+  const [code, setCode] = useState(
+    initialValue || '# Hello, world!\n\n<Button>Here is a button</Button>'
+  )
   return (
     <PlaygroundEditor
       code={code}
