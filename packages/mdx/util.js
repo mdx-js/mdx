fb6aa97bc25e00abeb32494209e30fe88bfe51ec
John Otander <johnotander@gmail.com>
2019-07-11 10:03:05 -0600

    Add param case tests, follow up for #624

--- packages/mdx/util.js
+++ packages/mdx/util.js
@@ -4,6 +4,12 @@ const EXPORT_DEFAULT_REGEX = /^export default/
 const BLOCKS_REGEX = '[a-z\\.]+(\\.){0,1}[a-z\\.]'
 const EMPTY_NEWLINE = '\n\n'
 
+const paramCase = string =>
+  string
+    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+    .replace(/([a-z])([0-9])/g, '$1-$2')
+    .toLowerCase()
+
 const toTemplateLiteral = text => {
   const escaped = text
     .replace(/\\/g, '\\\\') // Escape all "\" to avoid unwanted escaping in text nodes
@@ -18,4 +24,5 @@ module.exports.BLOCKS_REGEX = BLOCKS_REGEX
 module.exports.isImport = text => IMPORT_REGEX.test(text)
 module.exports.isExport = text => EXPORT_REGEX.test(text)
 module.exports.isExportDefault = text => EXPORT_DEFAULT_REGEX.test(text)
+module.exports.paramCase = paramCase
 module.exports.toTemplateLiteral = toTemplateLiteral
