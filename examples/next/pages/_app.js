ba7fba69c9ccad9e15b18a1d784ca563b13113b7
John Otander <johnotander@gmail.com>
2019-07-26 11:40:02 -0600

    Simplify next.js example
    Closes #685

--- examples/next/pages/_app.js
+++ examples/next/pages/_app.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import {MDXProvider} from '@mdx-js/tag'
-import {Provider, mdComponents} from 'unified-ui'
+import {MDXProvider} from '@mdx-js/react'
+
+const mdComponents = {
+  h1: props => <h1 style={{color: 'tomato'}} {...props} />
+}
 
 export default ({Component, pageProps}) => (
   <MDXProvider components={mdComponents}>
-    <Provider>
-      <Component {...pageProps} />
-    </Provider>
+    <Component {...pageProps} />
   </MDXProvider>
 )
