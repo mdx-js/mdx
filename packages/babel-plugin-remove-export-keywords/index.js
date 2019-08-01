3ac5491b8f0ad1c4cd7c873b7c0d5922f61a8526
John Otander <johnotander@gmail.com>
2019-08-01 13:02:29 -0600

    Run formatter on js file

--- packages/babel-plugin-remove-export-keywords/index.js
+++ packages/babel-plugin-remove-export-keywords/index.js
@@ -1,14 +1,14 @@
-module.exports = function removeExportKeywords() {
-    return {
-      visitor: {
-        ExportNamedDeclaration(path) {
-          const declaration = path.node.declaration;
-  
-          // Ignore "export { Foo as default }" syntax
-          if (declaration) {
-            path.replaceWith(declaration);
-          }
+module.exports = () => {
+  return {
+    visitor: {
+      ExportNamedDeclaration(path) {
+        const declaration = path.node.declaration
+
+        // Ignore "export { Foo as default }" syntax
+        if (declaration) {
+          path.replaceWith(declaration)
         }
       }
-    };
-};
+    }
+  }
+}
