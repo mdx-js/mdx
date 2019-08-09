c7e8d881b43600e703987c1372334c03576a7c18
John Otander <johnotander@gmail.com>
2019-08-08 18:33:06 -0600

    Add section to live editor guide around pragma usage (#722)
    * Add section to live editor guide around pragma usage
    
    Closes #571
    
    * Fix lint error

--- examples/syntax-highlighting/src/components/CodeBlock.js
+++ examples/syntax-highlighting/src/components/CodeBlock.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import Highlight, {defaultProps} from 'prism-react-renderer'
 import {LiveProvider, LiveEditor, LiveError, LivePreview} from 'react-live'
+import {mdx} from '@mdx-js/react'
 
 export default ({children, className, live, render}) => {
   const language = className.replace(/language-/, '')
@@ -10,7 +11,11 @@ export default ({children, className, live, render}) => {
   if (live) {
     return (
       <div style={{marginTop: '40px', backgroundColor: 'black'}}>
-        <LiveProvider code={children.trim()}>
+        <LiveProvider
+          code={children.trim()}
+          transformCode={code => '/** @jsx mdx */' + code}
+          scope={{mdx}}
+        >
           <LivePreview />
           <LiveEditor />
           <LiveError />
