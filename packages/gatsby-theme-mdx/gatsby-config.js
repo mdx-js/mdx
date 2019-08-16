be2eb9eb9a38de4c012da0eb26ae15289e279f8a
John Otander <johnotander@gmail.com>
2019-08-16 14:45:16 -0600

    Remove unused plugins

--- packages/gatsby-theme-mdx/gatsby-config.js
+++ packages/gatsby-theme-mdx/gatsby-config.js
@@ -4,7 +4,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-theme-ui',
-    'gatsby-plugin-emotion',
     'gatsby-plugin-catch-links',
     {
       resolve: 'gatsby-plugin-mdx',
