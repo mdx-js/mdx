3629146d4a5df4387f6151d4592071b4da94ec11
John Otander <johnotander@gmail.com>
2019-08-22 14:53:58 -0600

    Break out attribute coercion to its own package

---
+++ packages/babel-plugin-html-attributes-to-jsx/test/index.test.js
@@ -0,0 +1,22 @@
+const babel = require('@babel/core')
+
+const plugin = require('..')
+
+const testContents = `
+<img srcset="foo" />
+`
+const expectedResults = `React.createElement("img", {
+  srcSet: "foo"
+});`
+
+describe('babel-plugin-remove-export-keywords', () => {
+  test('removes all export keywords', () => {
+    const result = babel.transform(testContents, {
+      configFile: false,
+      plugins: [plugin],
+      presets: [require('@babel/preset-react')]
+    })
+
+    expect(result.code).toEqual(expectedResults)
+  })
+})
