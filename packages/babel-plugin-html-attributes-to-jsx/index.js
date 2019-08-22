945bf4ca5959e8b166d14507a271af70601d528c
John Otander <johnotander@gmail.com>
2019-08-22 14:53:58 -0600

    Break out attribute coercion to its own package

---
+++ packages/babel-plugin-html-attributes-to-jsx/index.js
@@ -0,0 +1,61 @@
+const styleToObject = require('style-to-object')
+const camelCaseCSS = require('camelcase-css')
+const t = require('@babel/types')
+const {paramCase: camelCase} = require('@mdx-js/util')
+
+const TRANSLATIONS = require('./translations')
+
+const propsKeysVisitor = {
+  ObjectProperty(node) {
+    if (node.node.key.extra.rawValue in TRANSLATIONS) {
+      node.node.key.value =
+        TRANSLATIONS[node.node.key.value] || node.node.key.value
+    }
+  }
+}
+
+const jsxAttributeFromHTMLAttributeVisitor = {
+  JSXAttribute(node) {
+    if (node.node.name.name in TRANSLATIONS) {
+      node.node.name.name = TRANSLATIONS[node.node.name.name]
+    } else if (node.node.name.name === `props`) {
+      node.traverse(propsKeysVisitor)
+    } else if (
+      node.node.name.name.includes(`-`) &&
+      !node.node.name.name.startsWith(`data`) &&
+      !node.node.name.name.startsWith(`aria`)
+    ) {
+      node.node.name.name = camelCase(node.node.name.name)
+    }
+
+    if (
+      node.node.name.name === `style` &&
+      node.node.value.type === `StringLiteral`
+      //      Node.node.value.type !== "JSXExpressionContainer"
+    ) {
+      let styleArray = []
+      styleToObject(node.node.value.extra.rawValue, function(name, value) {
+        styleArray.push([camelCaseCSS(name), value])
+      })
+      node.node.value = t.jSXExpressionContainer(
+        t.objectExpression(
+          styleArray.map(([key, value]) =>
+            t.objectProperty(t.StringLiteral(key), t.stringLiteral(value))
+          )
+        )
+      )
+    }
+  }
+}
+
+const babelPluginHtmlAttributesToJsx = () => {
+  return {
+    visitor: {
+      JSXElement(path) {
+        path.traverse(jsxAttributeFromHTMLAttributeVisitor)
+      }
+    }
+  }
+}
+
+module.exports = babelPluginHtmlAttributesToJsx
---
+++ packages/babel-plugin-html-attributes-to-jsx/test/index.test.js
@@ -0,0 +1,34 @@
+const babel = require('@babel/core')
+
+const plugin = require('..')
+
+const testContents = `
+export const foo = 'bar';
+/** @jsx mdx*/ export const _frontmatter = {
+  title: "Here's a title with the word export"
+};
+const MDXContent = function () {};
+export { MDXContent as default };
+`
+const expectedResults = `const foo = 'bar';
+/** @jsx mdx*/
+
+const _frontmatter = {
+  title: "Here's a title with the word export"
+};
+
+const MDXContent = function () {};
+
+export { MDXContent as default };`
+
+describe('babel-plugin-remove-export-keywords', () => {
+  test('removes all export keywords', () => {
+    const result = babel.transform(testContents, {
+      configFile: false,
+      plugins: [plugin],
+      presets: [require('@babel/preset-react')]
+    })
+
+    expect(result.code).toEqual(expectedResults)
+  })
+})
