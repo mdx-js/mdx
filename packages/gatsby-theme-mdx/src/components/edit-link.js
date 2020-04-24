c90bae1702410d3d1a088b52a36e972bc83f22c4
Ilya Medvedev <ilya@medvedev.im>
2020-04-24 10:28:53 +0300

    EditLink: Fix trailing slash in pathname

--- packages/gatsby-theme-mdx/src/components/edit-link.js
+++ packages/gatsby-theme-mdx/src/components/edit-link.js
@@ -8,7 +8,7 @@ export default () => (
   <Location>
     {({location}) => (
       <a
-        href={base + location.pathname + '.mdx'}
+        href={base + location.pathname.replace(/\/$/, '') + '.mdx'}
         css={css({
           display: 'inline-block',
           color: 'inherit',
