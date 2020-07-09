ec248020a90a9bc26a7674f513afd053ef3234e0
Tom Parker-Shemilt <palfrey@tevp.net>
2020-07-09 22:41:30 +0100

    Fix all the "prettier" errors

--- packages/gatsby-theme-mdx/src/components/large-text.js
+++ packages/gatsby-theme-mdx/src/components/large-text.js
@@ -1,8 +1,5 @@
 /* @jsx jsx */
-import { jsx } from 'theme-ui'
 
-export default ({ children }) => (
-  <div sx={{ p: { fontSize: 4 }, mb: 6}}>
-    {children}
-  </div>
+export default ({children}) => (
+  <div sx={{p: {fontSize: 4}, mb: 6}}>{children}</div>
 )
