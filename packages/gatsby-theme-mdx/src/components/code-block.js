4cd76c483569f5de30ec9abd02e6457f364424bf
Sam Robbins <samrobbinsgb@gmail.com>
2020-07-13 09:18:17 +0100

    Update code-block.js
    Fix eslint errors

--- packages/gatsby-theme-mdx/src/components/code-block.js
+++ packages/gatsby-theme-mdx/src/components/code-block.js
@@ -1,9 +1,8 @@
 import React, {useContext} from 'react'
 import {ThemeContext} from '@emotion/core'
 import Highlight, {defaultProps} from 'prism-react-renderer'
-import github from 'prism-react-renderer/themes/github';
+import github from 'prism-react-renderer/themes/github'
 export default ({children, language, className: outerClassName}) => {
-  const theme = useContext(ThemeContext)
   return (
     <Highlight
       {...defaultProps}
