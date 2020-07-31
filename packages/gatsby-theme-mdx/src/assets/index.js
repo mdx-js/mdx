9c17a13986e13efacaa0fc79b6eac896bd3e4aa5
John Otander <johnotander@gmail.com>
2020-07-31 10:49:34 -0600

    OG/Twitter preview (#1201)
    * Spike out first pass of conf page
    
    * Add sign up form spike, make linter happy
    
    * Add head to conf layout, add og image
    
    * Add specific conf og image
    
    * Run formatter
    
    * Linter/formatter is breaking frontmatter
    
    * Ignore conf files in eslint

--- packages/gatsby-theme-mdx/src/assets/index.js
+++ packages/gatsby-theme-mdx/src/assets/index.js
@@ -1,5 +1,7 @@
 import Favicon from './favicon.ico'
 import Favicon16 from './favicon-16x16.png'
 import Favicon32 from './favicon-32x32.png'
+import Og from './og.png'
+import OgConf from './og-conf.png'
 
-export {Favicon, Favicon16, Favicon32}
+export {Favicon, Favicon16, Favicon32, Og, OgConf}
