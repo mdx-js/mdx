0feee7b7d5751c8306415d51e35c60b1bce02a82
Christian Murphy <christian.murphy.42@gmail.com>
2020-08-24 09:29:19 -0700

    types: mdx preact typings

---
+++ packages/preact/types/mdx-preact-test.tsx
@@ -0,0 +1,36 @@
+import {h, ComponentChildren} from 'preact'
+import {useContext} from 'preact/hooks'
+import {
+  MDXProvider,
+  useMDXComponents,
+  withMDXComponents,
+  ComponentsProp,
+  MDXContext,
+  mdx
+} from '@mdx-js/preact'
+
+const H1 = ({children}: {children: ComponentChildren}) => <h1>{children}</h1>
+
+const MDXProvideExample = () => (
+  <MDXProvider components={{h1: H1}}>
+    <h1>Hello, world!</h1>
+  </MDXProvider>
+)
+
+const WithMDXComponentsExample = () =>
+  withMDXComponents(({components}: ComponentsProp) => {
+    components // $ExpectType ComponentDictionary | undefined
+    return <div />
+  })
+
+const UseMDXComponentsExample = () => {
+  useMDXComponents({h1: H1}) // $ExpectType ComponentDictionary
+  useMDXComponents(() => ({h1: H1})) // $ExpectType ComponentDictionary
+}
+
+const UseMDXContextExample = () => {
+  const {components} = useContext(MDXContext)
+  components // $ExpectType ComponentDictionary | undefined
+}
+
+const MDXCreateElementExample = () => mdx('mdx', {title: 'example'}, [])
