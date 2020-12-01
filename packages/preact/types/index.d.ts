8c922e40dfdcc28b0e6649a13fdc86e129e706d6
Christian Murphy <christian.murphy.42@gmail.com>
2020-12-01 10:59:45 -0700

    Add types to `@mdx-js/preact` (#1233)
    * types: mdx preact typings
    
    * docs: add preact to typescript documentation
    
    Co-authored-by: John Otander <johnotander@gmail.com>

---
+++ packages/preact/types/index.d.ts
@@ -0,0 +1,69 @@
+// TypeScript Version: 3.4
+
+import {
+  h,
+  Context,
+  AnyComponent,
+  FunctionComponent
+} from 'preact'
+
+/**
+ * Mapping of names for JSX components to React components
+ */
+interface ComponentDictionary {
+  [name: string]: AnyComponent<any>
+}
+
+/**
+ * Prop type that includes a component dictionary
+ */
+interface ComponentsProp {
+  /**
+   * Mapping of names for JSX components to React components
+   */
+  components?: ComponentDictionary,
+}
+
+/**
+ * Direct access to the MDX React Context
+ */
+declare const MDXContext: Context<ComponentsProp>
+
+/**
+ * Provider for MDX context
+ */
+declare const MDXProvider: FunctionComponent<ComponentsProp>
+
+/**
+ * Gets components from the MDX Context
+ *
+ * @param components additional components to include
+ * @returns all components from context with overrides from components parameter
+ */
+declare function useMDXComponents(
+  components: ComponentDictionary | (() => ComponentDictionary)
+): ComponentDictionary
+
+/**
+ * High order component that passes components prop to child
+ *
+ * @param child Component being wrapped
+ */
+declare function withMDXComponents(
+  child: AnyComponent<ComponentsProp>
+): ReactElement | null
+
+/**
+ * Preact hyperscript function wrapped with handler for MDX content
+ */
+declare const mdx: typeof h
+
+export {
+  ComponentDictionary,
+  ComponentsProp,
+  MDXContext,
+  MDXProvider,
+  useMDXComponents,
+  withMDXComponents,
+  mdx
+}
