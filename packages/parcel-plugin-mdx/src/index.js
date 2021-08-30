fe187288fbbe8492a59a0a2d8c9c1f4fc57e9061
Jack Bates <jack@nottheoilrig.com>
2021-08-30 14:58:36 -0700

    WIP

--- packages/parcel-plugin-mdx/src/index.js
+++
@@ -1,8 +0,0 @@
-import {createRequire} from 'module'
-
-export default function (bundler) {
-  bundler.addAssetType(
-    'mdx',
-    createRequire(import.meta.url).resolve('./MDXAsset.js')
-  )
-}
--- packages/parcel-plugin-mdx/test/Component.js
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export default props => <h1>{props.children}</h1>
