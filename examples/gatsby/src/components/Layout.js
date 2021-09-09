b14534af48e3c828e3630f16198d8db99994abb7
Titus Wormer <tituswormer@gmail.com>
2021-09-09 09:31:51 +0200

    Refactor to simplify gatsby example

--- examples/gatsby/src/components/Layout.js
+++ examples/gatsby/src/components/Layout.js
@@ -1,17 +1,1 @@
-import React from 'react'
-import {Container, baseStyles} from 'unified-ui'
-
-const Style = ({children}) => (
-  <style
-    dangerouslySetInnerHTML={{
-      __html: children
-    }}
-  />
-)
-
-export default props => (
-  <>
-    <Style>{baseStyles}</Style>
-    <Container {...props} />
-  </>
-)
+export default ({children}) => children
