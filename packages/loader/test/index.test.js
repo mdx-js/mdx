9d8f658b2fddf613c1784d57a804b3736661a0b5
Titus Wormer <tituswormer@gmail.com>
2021-09-14 15:32:10 +0200

    Add preact test to loader

--- packages/loader/test/index.test.js
+++ packages/loader/test/index.test.js
@@ -1,48 +1,86 @@
 /**
- * @typedef {import('react').FC} FC
+ * @typedef {import('react').FC} ReactFunctionComponent
+ * @typedef {import('preact').FunctionComponent<unknown>} PreactFunctionComponent
  */
 
-import {promisify} from 'util'
-import path from 'path'
 import {promises as fs} from 'fs'
+import {promisify} from 'util'
+import {URL, fileURLToPath} from 'url'
 import {test} from 'uvu'
 import * as assert from 'uvu/assert'
 import webpack from 'webpack'
 import React from 'react'
 import {renderToStaticMarkup} from 'react-dom/server.js'
+import {h} from 'preact'
+import {render} from 'preact-render-to-string'
 
 test('@mdx-js/loader', async () => {
-  const base = path.resolve(path.join('test'))
+  const base = new URL('./', import.meta.url)
 
   await fs.writeFile(
-    path.join(base, 'webpack.mdx'),
+    new URL('webpack.mdx', base),
     'export const Message = () => <>World!</>\n\n# Hello, <Message />'
   )
 
+  // React.
   await promisify(webpack)({
     // @ts-expect-error context does not exist on the webpack options types.
-    context: base,
+    context: fileURLToPath(base),
     entry: './webpack.mdx',
     mode: 'none',
-    module: {rules: [{test: /\.mdx$/, use: [path.resolve('./index.cjs')]}]},
-    output: {path: base, filename: 'webpack.cjs', libraryTarget: 'commonjs'}
+    module: {rules: [{test: /\.mdx$/, use: [fileURLToPath(new URL('../index.cjs', import.meta.url))]}]},
+    output: {path: fileURLToPath(base), filename: 'react.cjs', libraryTarget: 'commonjs'}
   })
 
   // One for ESM loading CJS, one for webpack.
-  const Content = /** @type {FC} */ (
+  const ContentReact = /** @type {ReactFunctionComponent} */ (
     /* @ts-expect-error file is dynamically generated */
     // type-coverage:ignore-next-line
-    (await import('./webpack.cjs')).default.default
+    (await import('./react.cjs')).default.default
   )
 
   assert.equal(
-    renderToStaticMarkup(React.createElement(Content)),
+    renderToStaticMarkup(React.createElement(ContentReact)),
     '<h1>Hello, World!</h1>',
-    'should compile'
+    'should compile (react)'
   )
 
-  await fs.unlink(path.join(base, 'webpack.mdx'))
-  await fs.unlink(path.join(base, 'webpack.cjs'))
+  await fs.unlink(new URL('react.cjs', base))
+
+  // Preact.
+  await promisify(webpack)({
+    // @ts-expect-error context does not exist on the webpack options types.
+    context: fileURLToPath(base),
+    entry: './webpack.mdx',
+    mode: 'none',
+    module: {rules: [{
+      test: /\.mdx$/,
+      use: [
+        {
+          loader: fileURLToPath(new URL('../index.cjs', import.meta.url)),
+          options: {jsxImportSource: 'preact'}
+        }
+      ]
+    }]},
+    output: {path: fileURLToPath(base), filename: 'preact.cjs', libraryTarget: 'commonjs'}
+  })
+
+  // One for ESM loading CJS, one for webpack.
+  const ContentPreact = /** @type {PreactFunctionComponent} */ (
+    /* @ts-expect-error file is dynamically generated */
+    // type-coverage:ignore-next-line
+    (await import('./preact.cjs')).default.default
+  )
+
+  assert.equal(
+    render(h(ContentPreact, {})),
+    '<h1>Hello, World!</h1>',
+    'should compile (preact)'
+  )
+
+  await fs.unlink(new URL('preact.cjs', base))
+
+  await fs.unlink(new URL('webpack.mdx', base))
 })
 
 
