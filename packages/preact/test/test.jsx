0262e202597378d9d653efbab8a94e238535f15a
Titus <tituswormer@gmail.com>
2021-09-16 11:45:23 +0200

    Update `@mdx-js/preact` (#1660)
    * Use ESM
    * Add JSDoc based types (and improve the types a bunch)
    * Mark `MDXContext` and `withMDXComponents` exports as deprecated
      (they still work)
    * Remove `mdx` export, which is no longer needed
    * Use `xdm` for a second

--- packages/preact/test/test.jsx
+++ packages/preact/test/test.jsx
@@ -1,95 +1,15 @@
 /* @jsx h */
 /* @jsxFrag Fragment */
 import {test} from 'uvu'
-import assert from 'uvu/assert'
-import path from 'path'
+import * as assert from 'uvu/assert'
 import {h, Fragment} from 'preact'
+import * as runtime from 'preact/jsx-runtime'
 import {render} from 'preact-render-to-string'
-import {transformAsync as babelTransform} from '@babel/core'
-import mdxTransform from '../../mdx'
-import {MDXProvider, withMDXComponents, mdx} from '../src'
-
-const run = async value => {
-  // Turn the serialized MDX code into serialized JSX…
-  const doc = await mdxTransform(value, {skipExport: true})
-
-  // …and that into serialized JS.
-  const {code} = await babelTransform(doc, {
-    configFile: false,
-    plugins: [
-      '@babel/plugin-transform-react-jsx',
-      path.resolve(__dirname, '../../babel-plugin-remove-export-keywords')
-    ]
-  })
-
-  // …and finally run it, returning the component.
-  // eslint-disable-next-line no-new-func
-  return new Function('mdx', `${code}; return MDXContent`)(mdx)
-}
-
-test('should evaluate MDX code', async () => {
-  const Content = await run('# hi')
-
-  assert.equal(render(<Content />), '<h1>hi</h1>')
-})
-
-test('should evaluate some more complex MDX code (text, inline)', async () => {
-  const Content = await run(
-    '*a* **b** `c` <abbr title="Markdown + JSX">MDX</abbr>'
-  )
-
-  assert.equal(
-    render(<Content />),
-    '<p><em>a</em> <strong>b</strong> <code>c</code> <abbr title="Markdown + JSX">MDX</abbr></p>'
-  )
-})
-
-test('should evaluate some more complex MDX code (flow, block)', async () => {
-  const Content = await run('***\n> * 1. a')
-
-  assert.equal(
-    render(<Content />),
-    '<hr /><blockquote><ul><li><ol><li>a</li></ol></li></ul></blockquote>'
-  )
-})
-
-test('should warn on missing components', async () => {
-  const Content = await run('<Component />')
-  const calls = []
-  const {warn} = console
-  console.warn = (...parameters) => {
-    calls.push(parameters)
-  }
-
-  assert.equal(render(<Content />), '')
-
-  assert.equal(calls, [
-    [
-      'Component `%s` was not imported, exported, or provided by MDXProvider as global scope',
-      'Component'
-    ]
-  ])
-
-  console.warn = warn
-})
-
-test('should support components defined in MDX', async () => {
-  const Content = await run('export const A = () => <b>!</b>\n\n<A />')
-
-  assert.equal(render(<Content />), '<b>!</b>')
-})
-
-test('should not crash if weird values could come from JSX', async () => {
-  // As JSX is function calls, that function can also be used directly in
-  // MDX. Definitely not a great idea, but it’s an easy way to pass in funky
-  // values.
-  const Content = await run('{mdx(1)}')
-
-  assert.equal(render(<Content />), '<1></1>')
-})
+import {evaluate} from 'xdm'
+import {MDXProvider, useMDXComponents, withMDXComponents} from '../index.js'
 
 test('should support `components` with `MDXProvider`', async () => {
-  const Content = await run('# hi')
+  const {default: Content} = await evaluate('# hi', {...runtime, useMDXComponents})
 
   assert.equal(
     render(
@@ -106,7 +26,7 @@ test('should support `components` with `MDXProvider`', async () => {
 })
 
 test('should support `wrapper` in `components`', async () => {
-  const Content = await run('# hi')
+  const {default: Content} = await evaluate('# hi', {...runtime, useMDXComponents})
 
   assert.equal(
     render(
@@ -122,27 +42,8 @@ test('should support `wrapper` in `components`', async () => {
   )
 })
 
-test('should support dots in component names (such as `ol.li`) for a direct child “selector”', async () => {
-  const Content = await run('* a\n1. b')
-
-  const OlLi = props => <li className="ordered" {...props} />
-
-  assert.equal(
-    render(
-      <MDXProvider
-        components={{
-          'ol.li': OlLi
-        }}
-      >
-        <Content />
-      </MDXProvider>
-    ),
-    '<ul><li>a</li></ul><ol><li class="ordered">b</li></ol>'
-  )
-})
-
 test('should combine components in nested `MDXProvider`s', async () => {
-  const Content = await run('# hi\n## hello')
+  const {default: Content} = await evaluate('# hi\n## hello', {...runtime, useMDXComponents})
 
   assert.equal(
     render(
@@ -161,12 +62,12 @@ test('should combine components in nested `MDXProvider`s', async () => {
         </MDXProvider>
       </MDXProvider>
     ),
-    '<h1 style="color: tomato;">hi</h1><h2 style="color: papayawhip;">hello</h2>'
+    '<h1 style="color: tomato;">hi</h1>\n<h2 style="color: papayawhip;">hello</h2>'
   )
 })
 
 test('should support components as a function', async () => {
-  const Content = await run('# hi\n## hello')
+  const {default: Content} = await evaluate('# hi\n## hello', {...runtime, useMDXComponents})
 
   assert.equal(
     render(
@@ -185,12 +86,12 @@ test('should support components as a function', async () => {
         </MDXProvider>
       </MDXProvider>
     ),
-    '<h1>hi</h1><h2 style="color: papayawhip;">hello</h2>'
+    '<h1>hi</h1>\n<h2 style="color: papayawhip;">hello</h2>'
   )
 })
 
 test('should support a `disableParentContext` prop (sandbox)', async () => {
-  const Content = await run('# hi')
+  const {default: Content} = await evaluate('# hi', {...runtime, useMDXComponents})
 
   assert.equal(
     render(
@@ -209,7 +110,7 @@ test('should support a `disableParentContext` prop (sandbox)', async () => {
 })
 
 test('should support `withComponents`', async () => {
-  const Content = await run('# hi\n## hello')
+  const {default: Content} = await evaluate('# hi\n## hello', {...runtime, useMDXComponents})
   const With = withMDXComponents(props => <>{props.children}</>)
 
   // To do: should this use the `h2` component too?
@@ -229,7 +130,7 @@ test('should support `withComponents`', async () => {
         </With>
       </MDXProvider>
     ),
-    '<h1 style="color: tomato;">hi</h1><h2>hello</h2>'
+    '<h1 style="color: tomato;">hi</h1>\n<h2>hello</h2>'
   )
 })
 
