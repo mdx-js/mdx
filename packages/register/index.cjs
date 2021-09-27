2736d079f4f7fd20d7a12b258da7e124d6e3cdd5
Titus <tituswormer@gmail.com>
2021-09-27 13:17:11 +0200

    Add `@mdx-js/register` (#1689)

---
+++ packages/register/index.cjs
@@ -0,0 +1,6 @@
+'use strict'
+
+const runtime = require('react/jsx-runtime')
+const register = require('./lib/index.cjs')
+
+register({...runtime})
---
+++ packages/register/test/index.test.cjs
@@ -0,0 +1,37 @@
+/**
+ * @typedef {import('react').FC} FC
+ */
+
+'use strict'
+
+const path = require('path')
+const fs = require('fs').promises
+const {test} = require('uvu')
+const assert = require('uvu/assert')
+const React = require('react')
+const {renderToStaticMarkup} = require('react-dom/server.js')
+
+test('@mdx-js/register', async () => {
+  const base = path.resolve(path.join('test'))
+
+  await fs.writeFile(
+    path.join(base, 'register.mdx'),
+    'export const Message = () => <>World!</>\n\n# Hello, <Message />'
+  )
+
+  // OMG, it works!
+  const Content = /** @type {FC} */ (
+    /* @ts-expect-error file is dynamically generated */
+    require('./register.mdx') // type-coverage:ignore-line
+  )
+
+  assert.equal(
+    renderToStaticMarkup(React.createElement(Content)),
+    '<h1>Hello, World!</h1>',
+    'should compile'
+  )
+
+  await fs.unlink(path.join(base, 'register.mdx'))
+})
+
+test.run()
