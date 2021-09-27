97967e919fc6d1fdc07b2bf15c26502ad7950029
Titus <tituswormer@gmail.com>
2021-09-27 10:50:57 +0200

    Add `@mdx-js/rollup` (#1687)

---
+++ packages/rollup/lib/index.js
@@ -0,0 +1,51 @@
+/**
+ * @typedef {import('@rollup/pluginutils').FilterPattern} FilterPattern
+ * @typedef {import('@mdx-js/mdx/lib/compile').CompileOptions} CompileOptions
+ * @typedef {import('rollup').Plugin} Plugin
+ *
+ * @typedef RollupPluginOptions
+ * @property {FilterPattern} [include]
+ *   List of picomatch patterns to include
+ * @property {FilterPattern} [exclude]
+ *   List of picomatch patterns to exclude
+ *
+ * @typedef {CompileOptions & RollupPluginOptions} Options
+ *
+ * @todo
+ *   Support `source-map` always?
+ */
+
+import {VFile} from 'vfile'
+import {createFilter} from '@rollup/pluginutils'
+import {createFormatAwareProcessors} from '@mdx-js/mdx/lib/util/create-format-aware-processors.js'
+
+/**
+ * Compile MDX w/ rollup.
+ *
+ * @param {Options} [options]
+ * @return {Plugin}
+ */
+export function rollup(options = {}) {
+  const {include, exclude, ...rest} = options
+  const {extnames, process} = createFormatAwareProcessors(rest)
+  const filter = createFilter(include, exclude)
+
+  return {
+    name: 'xdm',
+    // @ts-expect-error `map` is added if a `SourceMapGenerator` is passed in.
+    async transform(value, path) {
+      const file = new VFile({value, path})
+
+      if (
+        file.extname &&
+        filter(file.path) &&
+        extnames.includes(file.extname)
+      ) {
+        const compiled = await process(file)
+        return {code: String(compiled.value), map: compiled.map}
+        // V8 on Erbium.
+        /* c8 ignore next 2 */
+      }
+    }
+  }
+}
