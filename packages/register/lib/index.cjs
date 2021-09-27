65852048836b22c6a5f6c7a9f42f4a042b8c65c4
Titus Wormer <tituswormer@gmail.com>
2021-09-27 11:48:45 +0200

    Add `@mdx-js/register`

---
+++ packages/register/lib/index.cjs
@@ -0,0 +1,64 @@
+'use strict'
+
+const fs = require('fs')
+const deasync = require('deasync')
+
+const {runSync} = deasync(load)('@mdx-js/mdx/lib/run.js')
+const {createFormatAwareProcessors} = deasync(load)(
+  '@mdx-js/mdx/lib/util/create-format-aware-processors.js'
+)
+const {resolveEvaluateOptions} = deasync(load)(
+  '@mdx-js/mdx/lib/util/resolve-evaluate-options.js'
+)
+
+module.exports = register
+
+function register(options) {
+  const {compiletime, runtime} = resolveEvaluateOptions(options)
+  const {extnames, processSync} = createFormatAwareProcessors(compiletime)
+  let index = -1
+
+  while (++index < extnames.length) {
+    // eslint-disable-next-line node/no-deprecated-api
+    require.extensions[extnames[index]] = mdx
+  }
+
+  function mdx(module, path) {
+    const file = processSync(fs.readFileSync(path))
+    const result = runSync(file, runtime)
+    module.exports = result.default
+    module.loaded = true
+  }
+}
+
+function load(filePath, callback) {
+  let called
+
+  // Sometimes, the import hangs (see error message for reasons).
+  // To fix that, a timeout can be used.
+  // However, setting a timeout, results in `deasync` waiting for it, even
+  // in cases where the import is already settled!
+  // That’s why this number is pretty low.
+  setTimeout(timeout, 192)
+
+  function timeout() {
+    done(
+      new Error(
+        'Could not import:\n' +
+          "this error can occur when doing `require('@mdx-js/register')` in an async function: please move the require to the top or remove it and use `node -r @mdx-js/register …` instead\n" +
+          'this error can also occur if both `@mdx-js/register` and `@mdx-js/esm-loader` are used: please use one or the other'
+      )
+    )
+  }
+
+  import(filePath).then((module) => {
+    done(null, module)
+  }, done)
+
+  function done(error, result) {
+    if (called) return
+    called = true
+    clearTimeout(timeout)
+    callback(error, result)
+  }
+}
---
+++ packages/register/readme.md
@@ -0,0 +1,52 @@
+# `@mdx-js/register`
+
+[![Build][build-badge]][build]
+[![Downloads][downloads-badge]][downloads]
+[![Sponsors][sponsors-badge]][opencollective]
+[![Backers][backers-badge]][opencollective]
+[![Chat][chat-badge]][chat]
+
+Node hook to require [MDX][].
+
+## Contribute
+
+See [Contributing on `mdxjs.com`][contributing] for ways to get started.
+See [Support][] for ways to get help.
+
+This project has a [code of conduct][coc].
+By interacting with this repository, organization, or community you agree to
+abide by its terms.
+
+## License
+
+[MIT][] © [Titus Wormer][author]
+
+[build-badge]: https://github.com/mdx-js/mdx/workflows/CI/badge.svg
+
+[build]: https://github.com/mdx-js/mdx/actions
+
+[downloads-badge]: https://img.shields.io/npm/dm/@mdx-js/register.svg
+
+[downloads]: https://www.npmjs.com/package/@mdx-js/register
+
+[sponsors-badge]: https://opencollective.com/unified/sponsors/badge.svg
+
+[backers-badge]: https://opencollective.com/unified/backers/badge.svg
+
+[opencollective]: https://opencollective.com/unified
+
+[chat-badge]: https://img.shields.io/badge/chat-discussions-success.svg
+
+[chat]: https://github.com/mdx-js/mdx/discussions
+
+[contributing]: https://mdxjs.com/contributing
+
+[support]: https://mdxjs.com/support
+
+[coc]: https://github.com/mdx-js/.github/blob/master/code-of-conduct.md
+
+[mit]: license
+
+[author]: https://wooorm.com
+
+[mdx]: https://github.com/mdx-js/mdx
