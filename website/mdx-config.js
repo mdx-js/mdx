25213b8db92e39fec73e0ab658aa1886f8732329
Titus Wormer <tituswormer@gmail.com>
2021-10-09 12:30:05 +0200

    Fi symlinks

--- website/mdx-config.js
+++ website/mdx-config.js
@@ -110,13 +110,26 @@ function unifiedInferRemoteMeta() {
       file.data.meta || (file.data.meta = {})
     )
 
-    const url = new URL(
-      pathToFileURL(file.path)
-        .href.slice(config.input.href.length - 1)
+    const fileUrl = pathToFileURL(file.path)
+    const parts = fileUrl.href.slice(config.git.href.length - 1).split('/')
+    let fp
+
+    if (parts[1] === 'docs') {
+      fp = ('/' + parts.slice(2).join('/'))
         .replace(/\.server\.mdx$/, '/')
-        .replace(/\/index\/$/, '/'),
-      config.site
-    )
+        .replace(/\/index\/$/, '/')
+    } else {
+      // Symlinks, which we have to hack around.
+      if (parts[1] !== 'packages' || parts[parts.length - 1] !== 'readme.md') {
+        throw new Error(
+          'Expected symlinked file to match `/packages/*/readme.md`'
+        )
+      }
+
+      fp = parts.slice(0, -1).join('/') + '/'
+    }
+
+    const url = new URL(fp, config.site)
 
     meta.origin = url.origin
     meta.pathname = url.pathname
