7ca8498a04a7b39d7cc2bc8dc0857b48a3f521c9
Titus Wormer <tituswormer@gmail.com>
2021-10-12 09:48:29 +0200

    Add some more notes

--- docs/_component/note.server.js
+++ docs/_component/note.server.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
+const known = new Set(['info', 'legacy', 'important'])
+
 export const Note = (props) => {
   const {children, type} = props
   const className = ['note']
 
-  if (type === 'info') className.push(type)
+  if (known.has(type)) className.push(type)
+  else {
+    throw new Error('Unknown note type `' + type + '`')
+  }
 
   return (
     <div className={className.join(' ')}>
