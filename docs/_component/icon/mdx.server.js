3f8a2fb14dca8735789c80395a31a9973eb3c769
Titus Wormer <tituswormer@gmail.com>
2021-10-17 14:25:06 +0200

    Add active styles to logo

--- docs/_component/icon/mdx.server.js
+++ docs/_component/icon/mdx.server.js
@@ -17,22 +17,13 @@ export const Mdx = () => (
         x=".75"
         y=".75"
         rx="4.5"
-        fill="var(--fg)"
-        style={{transition: '200ms'}}
+        fill="currentColor"
       />
-      <g
-        fill="none"
-        stroke="var(--bg)"
-        strokeWidth="6"
-        style={{transition: '200ms'}}
-      >
+      <g fill="none" stroke="var(--bg)" strokeWidth="6">
         <path d="M16.5 44V19L30.25 32.75l14-14v25" />
         <path d="M70.5 40V10.75" />
         <path d="M57 27.25L70.5 40.75l13.5-13.5" />
-        <path
-          stroke="var(--mdx-yellow)"
-          d="M122.5 41.24L93.25 12M93.5 41.25L122.75 12"
-        />
+        <path d="M122.5 41.24L93.25 12M93.5 41.25L122.75 12" />
       </g>
     </g>
   </svg>
