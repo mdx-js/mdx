a92986a03712afb0cd6b0e83ddf84912fd35416b
Titus Wormer <tituswormer@gmail.com>
2021-10-17 19:56:37 +0200

    Improve nav, add reading time and such

--- docs/_component/layout.server.js
+++ docs/_component/layout.server.js
@@ -1,22 +1,193 @@
 import React from 'react'
 import {NavSite, NavSiteSkip} from './nav-site.server.js'
-import {FootArticle} from './foot-article.server.js'
 import {FootSite} from './foot-site.server.js'
+import {sortItems} from './sort.js'
+
+const dateTimeFormat = new Intl.DateTimeFormat('en', {dateStyle: 'long'})
 
 export const Layout = (props) => {
-  const {name, navTree, children} = props
+  const {name, navTree, ghUrl, meta = {}} = props
+  const [self, parent] = findSelfAndParent(navTree) || []
+  const siblings = parent
+    ? sortItems(parent.children, parent.data.navSortItems)
+    : []
+  const index = siblings.indexOf(self)
+  const previous = index === -1 ? undefined : siblings[index - 1]
+  const next = index === -1 ? undefined : siblings[index + 1]
+  const authors = meta.authors || []
+  const time = (self ? accumulateReadingTime(self) : []).map((d) =>
+    Math.ceil(d)
+  )
+  let timeLabel
+
+  if (time.length > 1 && time[0] !== time[1]) {
+    timeLabel = time[0] + '-' + time[1] + ' minutes'
+  } else if (time[0]) {
+    timeLabel = time[0] + ' minute' + (time[0] > 1 ? 's' : '')
+  }
+
+  function accumulateReadingTime(d) {
+    const time = (d.data.meta || {}).readingTime
+    const total = time ? (Array.isArray(time) ? time : [time, time]) : []
+
+    let index = -1
+    while (++index < d.children.length) {
+      const childTime = accumulateReadingTime(d.children[0])
+      if (childTime[0]) total[0] = (total[0] || 0) + childTime[0]
+      if (childTime[1]) total[1] = (total[1] || 0) + childTime[1]
+    }
+
+    return total
+  }
+
+  function entryToTitle(d) {
+    return (d.data.matter || {}).title || (d.data.meta || {}).title
+  }
+
+  const up =
+    parent && self && parent !== navTree ? (
+      <div>
+        <a href={parent.name}>{entryToTitle(parent)}</a>
+        {' / '}
+        <a href={name} aria-current="page">
+          {entryToTitle(self)}
+        </a>
+      </div>
+    ) : undefined
+
+  const back = previous ? (
+    <div>
+      Previous:
+      <br />
+      <a rel="prev" href={previous.name}>
+        {entryToTitle(previous)}
+      </a>
+    </div>
+  ) : undefined
+
+  const forward = next ? (
+    <div>
+      Next:
+      <br />
+      <a rel="next" href={next.name}>
+        {entryToTitle(next)}
+      </a>
+    </div>
+  ) : undefined
+
+  const edit = (
+    <div>
+      Found a typo? Other suggestions?
+      <br />
+      <a href={ghUrl.href}>Edit this page on GitHub</a>
+    </div>
+  )
+
+  const published = meta.published ? (
+    <>Published on {dateTimeFormat.format(meta.published)}</>
+  ) : undefined
+
+  const modified = meta.modified ? (
+    <>Modified on {dateTimeFormat.format(meta.modified)}</>
+  ) : undefined
+
+  const date =
+    published || modified ? (
+      <div>
+        {published}
+        {published && modified ? <br /> : undefined}
+        {modified}
+      </div>
+    ) : undefined
+
+  const readingTime = timeLabel ? <>{timeLabel} read</> : undefined
+
+  const creditsList = authors.map((d, i) => {
+    const href = d.github
+      ? 'https://github.com/' + d.github
+      : d.twitter
+      ? 'https://twitter.com/' + d.twitter
+      : d.url
+      ? d.url
+      : undefined
+    return (
+      <span key={d.name}>
+        {i ? ', ' : ''}
+        {href ? <a href={href}>{d.name}</a> : d.name}
+      </span>
+    )
+  })
+
+  const credits = creditsList.length > 0 ? <>By {creditsList}</> : undefined
+
+  const info =
+    readingTime || credits ? (
+      <>
+        {readingTime}
+        {readingTime && credits ? <br /> : undefined}
+        {credits}
+      </>
+    ) : undefined
+
+  const header =
+    up || info ? (
+      <div className="block article-row">
+        {up ? <div className="article-row-start">{up}</div> : undefined}
+        {info ? <div className="article-row-end">{info}</div> : undefined}
+      </div>
+    ) : undefined
+
+  const tail =
+    edit || date ? (
+      <div className="block article-row">
+        {edit ? <div className="article-row-start">{edit}</div> : undefined}
+        {date ? <div className="article-row-end">{date}</div> : undefined}
+      </div>
+    ) : undefined
+
+  const footer =
+    back || forward ? (
+      <div className="block article-row">
+        {back ? <div className="article-row-start">{back}</div> : undefined}
+        {forward ? <div className="article-row-end">{forward}</div> : undefined}
+      </div>
+    ) : undefined
 
   return (
     <div className="page doc">
       <NavSiteSkip />
       <main>
         <article>
-          <div className="content body">{children}</div>
-          <FootArticle {...props} />
+          {header ? (
+            <header className="content">
+              <div className="block head-article">{header}</div>
+            </header>
+          ) : undefined}
+          <div className="content body">{props.children}</div>
+          {footer || tail ? (
+            <footer className="content">
+              <div className="block foot-article">
+                {footer}
+                {tail}
+              </div>
+            </footer>
+          ) : undefined}
         </article>
         <FootSite />
       </main>
       <NavSite name={name} navTree={navTree} />
     </div>
   )
+
+  function findSelfAndParent(item, parent) {
+    if (item.name === name) return [item, parent]
+
+    let index = -1
+
+    while (++index < item.children.length) {
+      const result = findSelfAndParent(item.children[index], item)
+
+      if (result) return result
+    }
+  }
 }
