cd95df6dbfea18d444d5895fe6d71d2280a65e3e
Yordis Prieto <yordis.prieto@gmail.com>
2021-11-13 04:29:37 -0500

    Add `run`, `runSync` exports (#1792)
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

--- packages/mdx/index.js
+++ packages/mdx/index.js
@@ -7,4 +7,5 @@
 export {createProcessor} from './lib/core.js'
 export {compile, compileSync} from './lib/compile.js'
 export {evaluate, evaluateSync} from './lib/evaluate.js'
+export {run, runSync} from './lib/run.js'
 export {nodeTypes} from './lib/node-types.js'
