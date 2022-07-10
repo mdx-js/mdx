1d7a140c9b03bb5abc85652f270b32306d5d11f5
Dimitri POSTOLOV <en3m@ya.ru>
2022-07-10 12:28:26 +0200

    Update index.cjs

--- packages/loader/index.cjs
+++ packages/loader/index.cjs
@@ -10,7 +10,7 @@
 module.exports = function (code) {
   const callback = this.async()
   // Note that `import()` caches, so this should be fast enough.
-  import('./lib/index.js').then((module) =>
-    module.loader.call(this, code, callback)
+  import('./lib/index.js').then((mod) =>
+    mod.loader.call(this, code, callback)
   )
 }
