11b96d3c802e9e50fa73bf0c46e264b7bcc3b25f
bholmesdev <hey@bholmes.dev>
2022-08-11 19:18:24 -0400

    feat: add toValidIdentifierName util

---
+++ packages/mdx/lib/util/to-valid-identifier-name.js
@@ -0,0 +1,16 @@
+import {start, cont} from 'estree-util-is-identifier-name'
+
+/**
+ * Replace all invalid identifier characters with underscores "_"
+ * @param {string} str
+ */
+export function toValidIdentifierName(string_) {
+  if (string_.length === 0) return '_'
+  let validString = ''
+  validString += start(string_[0].charCodeAt(0)) ? string_[0] : '_'
+  for (const char of string_.slice(1)) {
+    validString += cont(char.charCodeAt(0)) ? char : '_'
+  }
+
+  return validString
+}
