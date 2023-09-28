ae6fc6e270e86ed48ca40e388a73b375251bb8a6
Titus Wormer <tituswormer@gmail.com>
2023-09-28 14:41:16 +0200

    Remove webpack

--- website/loader.js
+++ website/loader.js
@@ -3,5 +3,4 @@ import {createLoader as createJsxLoader} from '../script/jsx-loader.js'
 import config from './mdx-config.js'
 
 const loader = {loaders: [createJsxLoader(), createMdxLoader(config)]}
-
 export default loader
