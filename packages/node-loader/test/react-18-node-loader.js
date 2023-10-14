8f63c0502ee7d5984f12a2b10dcba5b7d64925ce
Titus Wormer <tituswormer@gmail.com>
2023-10-14 15:39:06 +0200

    Refactor `package.json`s

--- packages/node-loader/test/react-18-node-loader.js
+++ packages/node-loader/test/react-18-node-loader.js
@@ -1,8 +1,6 @@
 import {createLoader} from '../index.js'
 
-// Load is for Node 17+, the rest for 12, 14, 16.
-const {load, getFormat, transformSource} = createLoader({
-  fixRuntimeWithoutExportMap: false
-})
+// To do: break to not fix by default, remove this file.
+const {load} = createLoader({fixRuntimeWithoutExportMap: false})
 
-export {load, getFormat, transformSource}
+export {load}
