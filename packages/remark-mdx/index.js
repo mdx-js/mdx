070f349181c3f9caa3a1a972eee09b2d0ea21832
Titus Wormer <tituswormer@gmail.com>
2023-10-16 12:18:02 +0200

    Refactor to move code to `lib/`

--- packages/remark-mdx/index.js
+++ packages/remark-mdx/index.js
@@ -1,45 +1,8 @@
-/**
- * @typedef {import('unified').Processor} Processor
- * @typedef {import('micromark-extension-mdxjs').Options} MicromarkOptions
- * @typedef {import('mdast-util-mdx').ToMarkdownOptions} ToMarkdownOptions
- * @typedef {import('mdast-util-mdx')} DoNotTouchAsThisIncludesMdxInTree
- */
+// Augment node types:
+/// <reference types="mdast-util-mdx" />
 
 /**
- * @typedef {MicromarkOptions & ToMarkdownOptions} Options
- *   Configuration.
+ * @typedef {import('./lib/index.js').Options} Options
  */
 
-import {mdxjs} from 'micromark-extension-mdxjs'
-import {mdxFromMarkdown, mdxToMarkdown} from 'mdast-util-mdx'
-
-/** @type {Options} */
-const emptyOptions = {}
-
-/**
- * Plugin to support MDX (import/exports: `export {x} from 'y'`; expressions:
- * `{1 + 1}`; and JSX: `<Video id={123} />`).
- *
- * @param {Options | null | undefined} [options]
- *   Configuration (optional).
- * @returns {undefined}
- *   Nothing.
- */
-export default function remarkMdx(options) {
-  // @ts-expect-error: TS is wrong about `this`.
-  // eslint-disable-next-line unicorn/no-this-assignment
-  const self = /** @type {Processor} */ (this)
-  const settings = options || emptyOptions
-  const data = self.data()
-
-  const micromarkExtensions =
-    data.micromarkExtensions || (data.micromarkExtensions = [])
-  const fromMarkdownExtensions =
-    data.fromMarkdownExtensions || (data.fromMarkdownExtensions = [])
-  const toMarkdownExtensions =
-    data.toMarkdownExtensions || (data.toMarkdownExtensions = [])
-
-  micromarkExtensions.push(mdxjs(settings))
-  fromMarkdownExtensions.push(mdxFromMarkdown())
-  toMarkdownExtensions.push(mdxToMarkdown(settings))
-}
+export {default} from './lib/index.js'
