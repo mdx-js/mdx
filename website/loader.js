c5a07298a9305263ae8d7ad9290f5ea69c9812f1
Titus Wormer <tituswormer@gmail.com>
2025-11-17 20:10:39 +0100

    Use Node 20 in Actions

--- website/loader.js
+++ website/loader.js
@@ -1,11 +1,4 @@
-import {createLoader as createJsxLoader} from '../script/jsx-loader.js'
-import {initialize, load} from './mdx-loader.js'
+import {register} from 'node:module'
 
-const loader = {loaders: [createJsxLoader(), {initialize, load}]}
-export default loader
-
-// To do: after dropping Node 18:
-// import {register} from 'node:module'
-
-// register('../script/jsx-loader.js', import.meta.url)
-// register('./mdx-loader.js', import.meta.url)
+register('../script/jsx-loader.js', import.meta.url)
+register('./mdx-loader.js', import.meta.url)
